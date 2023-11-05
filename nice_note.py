76840ff220d24427c42a759540503092aab2ebec
Guijin Ding <dingguijin@gmail.com>
2023-11-05 20:08:39 +0800

    id

--- nice_note.py
+++ nice_note.py
@@ -1,17 +1,25 @@
 import tornado
 import asyncio
 import time
+import json
 
+_id = 0
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         print("Ignore Get", self.request)
-        self.write("Nice")
+        global _id
+        _id += 1
+        _r = {"id":_id,"jsonrpc":"2.0","result":"0x1"}
+        self.write(json.dumps(_r))
 
     def post(self):
         print(self.request.body)
-        with open("note-%s-note" % time.time(), "w") as _f:
+        with open("note-%s-note" % time.time(), "wb") as _f:
             _f.write(self.request.body)
-        self.write(json.dumps({"success":True}))
+        global _id
+        _id += 1
+        _r = {"id":_id,"jsonrpc":"2.0","result":"0x1"}
+        self.write(json.dumps(_r))
 
 def make_app():
     return tornado.web.Application([
