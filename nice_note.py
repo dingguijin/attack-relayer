8c294240a098290da732afd2f4be28a31f4a8638
Guijin Ding <dingguijin@gmail.com>
2023-11-05 11:37:20 +0800

    nice note

---
+++ nice_note.py
@@ -0,0 +1,26 @@
+import tornado
+import asyncio
+import time
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Nice")
+
+    def post(self):
+        print(self.request.body)
+        with open "note-%s-note" % time.time() as _f:
+            _f.write(self.request.body)
+        self.write(json.dumps({"success":True}))
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+async def main():
+    app = make_app()
+    app.listen(8888)
+    await asyncio.Event().wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
